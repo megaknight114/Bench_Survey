7f27404c3de3a3d3bb370666407ebe01ab7f83c5
holomegaknight <xzh20070603@163.com>
2025-12-21 12:39:24 +0800

    Update APPS_SCRIPT_URL to deployed Web App URL

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // Get this URL after deploying your Apps Script as a Web App
   // Format: https://script.google.com/macros/s/AKfycbxxxxx/exec
-  APPS_SCRIPT_URL: 'YOUR_APPS_SCRIPT_URL',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw-sMB5nGELbDM-C1feUV5IN4Omgpvm8OmVcRGZPMoTmQkP5_4CAH-Q1bb4ARVj3GEA/exec',
   
   // Optional: If you host texts.json elsewhere, change this URL
   TEXTS_JSON_URL: './texts.json'
