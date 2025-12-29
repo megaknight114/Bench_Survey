4b7731f6879cf297bd4826db9d5f78f10ae15c26
holomegaknight <xzh20070603@163.com>
2025-12-29 17:19:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const TEXTS_VERSION = '2025-12-29-02-07';
 const CONFIG = {
   // Get this URL after deploying your Apps Script as a Web App
   // Format: https://script.google.com/macros/s/AKfycbxxxxx/exec
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxt4XhPfsQj0z6cnPYPtkgKkJXg1qAurne4CbRHD_Ct9EELwlJ4fC_43Zl2Bl4Ti9gc/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwBNQ7IFOg0-nojBCQdVKsfCrcc-3kO0qDLY5mgV_Qs3WoggRJpWZPGjPKmMmCKNaja/exec',
   
   // Optional: If you host texts.json elsewhere, change this URL
   // Add ?v=... to avoid stale GitHub Pages / browser caches.
